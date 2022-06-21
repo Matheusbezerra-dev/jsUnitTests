64479efc38ae29894b40c8a22bb3c0750e465c02
Matheus Bezerra <mabezerra2@gmail.com>
2022-06-20 20:33:00 -0400

    finalização do exercicio 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,22 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    expect(typeof productDetails === 'function' ).toBe(true);
+    expect(typeof productDetails('Alcool gel', 'Máscara' === 'object')).toBe('object');
+    expect(productDetails.length).toBe(2);
+    expect(typeof (productDetails('Alcool gel', 'Máscara')[0])).toBe('object');
+    expect(typeof (productDetails('Alcool gel', 'Máscara')[1])).toBe('object');
+    expect(productDetails('Alcool gel', 'Máscara')[0]).not.toEqual(productDetails('Alcool gel', 'Máscara')[1]);
+    const productIdsOne = productDetails('Alcool gel', 'Máscara')[0].details.productId;
+    const productIdsTwo = productDetails('Alcool gel', 'Máscara')[1].details.productId;
+    expect(productIdsOne.endsWith('123')).toBe(true);
+    expect(productIdsTwo.endsWith('123')).toBe(true);
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    // Teste se productDetails é uma função.1
+    // Teste se o retorno da função é um array.2
+    // Teste se o array retornado pela função contém dois itens dentro.3
+    // Teste se os dois itens dentro do array retornado pela função são objetos.4
+    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.5
     // Teste se os dois productIds terminam com 123.
   });
 });
